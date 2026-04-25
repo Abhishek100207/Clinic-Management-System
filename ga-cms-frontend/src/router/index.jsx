3920a374dab34a26524f6dc158c7ede50f56260b
vaishnavi <phularivaishnavi2004@gmail.com>
2026-04-25 19:11:40 +0530

    changes made on signin/signup

--- ga-cms-frontend/src/router/index.jsx
+++ ga-cms-frontend/src/router/index.jsx
@@ -7,80 +7,36 @@ import SignUpPage from '../components/auth/SignUpPage';
 import UnauthorizedPage from '../components/auth/UnauthorizedPage';
 import AppShell from '../components/layout/AppShell';
 
-import HomepageDashboard from '../components/dashboards/HomepageDashboard';
 import AdminDashboard from '../components/dashboards/AdminDashboard';
 import DoctorDashboard from '../components/dashboards/DoctorDashboard';
 import ReceptionistDashboard from '../components/dashboards/ReceptionistDashboard';
 import TechnicianDashboard from '../components/dashboards/TechnicianDashboard';
 import PatientRegistrationForm from '../components/patients/PatientRegistrationForm';
+import AddStaffPage from '../components/staff/AddStaffPage';
+import ChangePasswordPage from '../components/auth/ChangePasswordPage';
 
 const AppRouter = () => {
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-=======
-      <Route path="/" element={<HomepageDashboard />} />
-      <Route path="/login" element={<LoginPage />} />
->>>>>>> 3b67c0aebbc1e22f5c8c14e5827d7627e468a33e
       <Route path="/unauthorized" element={<UnauthorizedPage />} />
 
-
-      {/* Protected Routes encapsulated in AppShell */}
       <Route element={<AppShell />}>
-        {/* Admin / Senior Doctor Routes */}
-        <Route 
-          path="/dashboard/admin" 
-          element={
-            <AuthGuard allowedRoles={['senior_doctor']}>
-              <AdminDashboard />
-            </AuthGuard>
-          } 
-        />
-
-        {/* Doctor Routes */}
-        <Route 
-          path="/dashboard/doctor" 
-          element={
-            <AuthGuard allowedRoles={['doctor', 'senior_doctor']}>
-              <DoctorDashboard />
-            </AuthGuard>
-          } 
-        />
-
-        {/* Receptionist Routes */}
-        <Route 
-          path="/dashboard/receptionist" 
-          element={
-            <AuthGuard allowedRoles={['receptionist']}>
-              <ReceptionistDashboard />
-            </AuthGuard>
-          } 
-        />
-
-        {/* Technician Routes */}
-        <Route 
-          path="/dashboard/technician" 
-          element={
-            <AuthGuard allowedRoles={['technician']}>
-              <TechnicianDashboard />
-            </AuthGuard>
-          } 
-        />
-
-        {/* Shared Routes (e.g. Patient Registration) */}
-        <Route 
-          path="/register-patient" 
-          element={
-            <AuthGuard allowedRoles={['receptionist', 'doctor', 'senior_doctor']}>
-              <PatientRegistrationForm />
-            </AuthGuard>
-          } 
-        />
-        
-        {/* Catch-all for stubbed routes to redirect to their dashboard or show something */}
+        <Route path="/dashboard/admin"
+          element={<AuthGuard allowedRoles={['senior_doctor']}><AdminDashboard /></AuthGuard>} />
+        <Route path="/dashboard/doctor"
+          element={<AuthGuard allowedRoles={['doctor','senior_doctor']}><DoctorDashboard /></AuthGuard>} />
+        <Route path="/dashboard/receptionist"
+          element={<AuthGuard allowedRoles={['receptionist']}><ReceptionistDashboard /></AuthGuard>} />
+        <Route path="/dashboard/technician"
+          element={<AuthGuard allowedRoles={['technician']}><TechnicianDashboard /></AuthGuard>} />
+        <Route path="/register-patient"
+          element={<AuthGuard allowedRoles={['receptionist','doctor','senior_doctor']}><PatientRegistrationForm /></AuthGuard>} />
+        <Route path="/staff/add"
+          element={<AuthGuard allowedRoles={['doctor','senior_doctor']}><AddStaffPage /></AuthGuard>} />
+        <Route path="/change-password" element={<ChangePasswordPage />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Route>
     </Routes>
